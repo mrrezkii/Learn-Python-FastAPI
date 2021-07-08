efc596e583afa320967b21d322bb4f8789e19d6f
nando <muhammad.rezki.ananda@gmail.com>
2021-07-08 21:20:02 +0700

    Query parameters

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data' : 'blog list'}
+@app.get('/blog')
+def index(limit = 10, published : bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data' : f'{limit} published blogs from the db'}
+    else:
+        return {'data' : f'{limit} blogs from the db'}
 
 @app.get('/blog/unpublished')
 def unpublished():
@@ -12,9 +16,8 @@ def unpublished():
 
 @app.get('/blog/{id}')
 def show(id : int):
-    # fetch blog with id = id
     return {'data' : id} 
 
 @app.get('/blog/{id}/comments')
-def comments(id):
+def comments(id, limit = 10):
     return {'data' : {'1', '2'}}
